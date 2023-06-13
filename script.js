d415ee3fdff5c80b78d06c3418ed7fe52a722c22
LuisTul <luis360123@gmail.com>
2023-06-13 16:59:51 -0600

    Clase 03 completada

--- script.js
+++ script.js
@@ -4,15 +4,20 @@ const createTask = (evento)=>{
     evento.preventDefault();
     const input = document.querySelector("[data-form-input]");
     const value = input.value;
-    const task = document.querySelector("[data-task]");
+    const list = document.querySelector("[data-list]");
+    const task = document.createElement("li");
+    task.classList.add("card");
     input.value = '';
     //backticks
     const content = `<div>
     <i class="far fa-check-square icon"></i>
     <span class="task">${value}</span>
-  </div>
-  <i class="fas fa-trash-alt trashIcon icon"></i>`;
-  task.innerHTML = content;
+    </div>
+    <i class="fas fa-trash-alt trashIcon icon"></i>`;
+    task.innerHTML = content;
+
+    list.appendChild(task);
+
     console.log(content);
 }
 
